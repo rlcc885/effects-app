695626abd828944b179ac343ce225612a91a0444
Renzo Chávez <rlchavezc@outlook.com>
2020-03-20 17:33:18 -0500

    - Added Services, Models, Modules & Routing

---
+++ src/app/services/usuario.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsuarioService {
+
+  private url:string = 'https://reqres.in/api';
+
+  constructor( private http: HttpClient) { }
+
+  getUsers() {
+    return this.http.get( `${ this.url }/users?perpgage=6` )
+      .pipe(
+        map( resp => resp['data'] )
+      );
+  }
+}
